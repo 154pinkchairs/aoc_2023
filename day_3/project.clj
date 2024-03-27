5e3906ef756e9ce8e68f501a6fdef705f9189a24
Marcelina Hołub <mholub@tutanota.com>
2024-03-27 01:32:53 +0100

    WIP(day3): solution to part 1 compiles, but is wrong

---
+++ day_3/project.clj
@@ -0,0 +1,8 @@
+(defproject default "0.1.0-SNAPSHOT"
+  :description "Part one"
+  :url "http://github.com/mjholub/aoc_2023"
+  :license {:name "Beerware"
+            :url "https://en.wikipedia.org/wiki/Beerware"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns part1.core}
+  :main part1.core)
---
+++ day_3/src/part1/core.clj
@@ -0,0 +1,35 @@
+(ns part1.core
+  (:gen-class)
+  (:import [java.io BufferedReader FileReader]))
+
+(defn read-schematic [^String filename]
+  (with-open [rdr (BufferedReader. (FileReader. filename))]
+    (doall (line-seq rdr))))
+
+(defn is-part-number? [schematic symbols x y]
+  (let [directions [[-1 0] [1 0] [0 -1] [0 1] [-1 -1] [-1 1] [1 -1] [1 1]]
+        height (count schematic)
+        width (count (first schematic))
+        in-range? (fn [x y] (and (<= 0 x (dec height)) (<= 0 y (dec width))))
+        cell (get (get schematic x) y)]
+    (and cell
+         (Character/isDigit ^Character cell)
+         (some #(let [[dx dy] %
+                      [nx ny] [(+ x dx) (+ y dy)]]
+                  (and (in-range? nx ny)
+                       (contains? symbols (get (get schematic nx) ny))))
+               directions))))
+
+(defn sum-part-numbers [^String filename]
+  (let [schematic (read-schematic filename)
+        symbols (set (filter #(not (or (Character/isDigit ^Character %) (= \. ^Character %)))
+                             (apply concat schematic)))
+        ; declare pairs of all 8 possible directions on a 2D grid
+        height (count schematic)
+        width (count (first schematic))]
+    (reduce + (for [x (range height)
+                    y (range width)
+                    :when (is-part-number? schematic symbols x y)]
+                (- (int (get (get schematic x) y)) 48)))))
+
+(println (sum-part-numbers "input.txt"))
