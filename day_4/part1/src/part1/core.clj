c6a740c8ff992a90a510114f155da5481f5b6e39
Marcelina Hołub <mholub@tutanota.com>
2024-03-29 22:15:17 +0100

    fix: day 4.1 solution

--- day_4/part1/src/part1/core.clj
+++ day_4/part1/src/part1/core.clj
@@ -2,9 +2,6 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
-(comment
-  "too high :(")
-
 (defn parse-card [line]
   (when line
     ; cut Card$number:
@@ -12,9 +9,6 @@
           ; trim potential excess whitespace(s) from the card numbers
           winning-numbers (str/replace (first (str/split cleaned #"\|")) #"\s+" " ")
           card-numbers (str/replace (second (str/split cleaned #"\|")) #"\s+" " ")]
-      ; debug print
-      (println "winning-numbers: " winning-numbers)
-      (println "card-numbers: " card-numbers)
       (if (nil? card-numbers)
         (throw (ex-info "Invalid input" {:line cleaned}))
         (let [winning-set (set (map #(Integer/parseInt %) (filter seq (str/split winning-numbers #" "))))
@@ -28,7 +22,7 @@
       0
       ; calculate points according to the game rules where 1st match gives 1 pt
       ; then each match after that doubles the point value of a card
-      (let [points (reduce (fn [acc _] (* 2 acc)) 1 (range (count matches)))]
+      (let [points (Math/pow 2 (dec (count matches)))]
         points))))
 
 (defn solve [filename]
@@ -38,4 +32,4 @@
          (map calc-points)
          (reduce +))))
 
-(println (solve "head.txt"))
+(println (solve "input.txt"))
